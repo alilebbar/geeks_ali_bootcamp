ed6796d3e59d8ca048b8a81f94d13a1e6a47791f
alilebbar <alilebbar94@gmail.com>
2025-05-12 21:23:00 +0100

    week 2 day 1 challenges

---
+++ week_2/day1/challenges/ex1.sql
@@ -0,0 +1,6 @@
+select actors_id from actors;
+/* il exicte 7 acteurs*/
+insert into actors (first_name,last_name,birthdate,number_oscars)
+values ('','','',)
+
+/* le resultat est erreur car on a mentionner dans la declaration de c'est champs NOT NULL*/
